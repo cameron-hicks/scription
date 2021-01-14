e7ffb74c29b9777b868ac729ceb1e14df6dea725
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-01-13 23:19:36 -0500

    finished Create UX

--- src/components/ScrCreator.jsx
+++ src/components/ScrCreator.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import ABCJS from 'abcjs';
 
 const ScrCreator = () => { 
   const [newName, setName] = useState('');
   const [newTitle, setTitle] = useState('');
   const [newGenre, setGenre] = useState('');
   const [newAbc, setAbc] = useState('');
+  const [previewRendered, setRendered ] = useState(false);
+
+  useEffect(() => {
+    //invoke ABCJS.renderAbc AFTER the component has mounted/updated
+    ABCJS.renderAbc('preview', newAbc, { responsive: 'resize' });
+    return setRendered(true);
+  }, [previewRendered, newAbc]);
+
   
   return (
     <div className="ScrCreator">
       <form>
-        <input id="username-input" type="text" placeholder="username" onChange={() => setName(this.value)}></input>
-        <input id="title-input" type="text" placeholder="song title" onChange={() => setTitle(this.value)}></input>
-        <select id="genre-input" defaultValue="" onChange={() => setGenre(this.value)}>
+        <input id="username-input" type="text" placeholder="username" onChange={(event) => setName(event.target.value)}></input>
+        <input id="title-input" type="text" placeholder="song title" onChange={(event) => setTitle(event.target.value)}></input>
+        <select id="genre-input" defaultValue="" onChange={(event) => setGenre(event.target.value)}>
           <option value="" disabled>genre</option>
           <option value="folk">folk</option>
           <option value="pop">pop</option>
           <option value="movie music">movie music</option>
           <option value="other">other</option>
         </select>
-        <textarea spellCheck="false" placeholder="Add your transcription in ABC notation..." onChange={() => setAbc(this.value)}></textarea>
+
+        <textarea spellCheck="false" 
+          placeholder="Add your transcription in ABC notation..." 
+          onChange={(event) => {
+            setAbc(event.target.value);
+            setRendered(false);
+          }}
+        >
+        </textarea>
+        <div id="preview"></div>
+
         <div className="right-align-btns">
           <button onClick={() => console.log(newName, newTitle, newGenre, newAbc)}>
             post <i className="fas fa-share-square"></i>
