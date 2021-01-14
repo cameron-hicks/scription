0d9e694cb77e3297747960e4ab422296d2a0b865
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-01-14 17:48:21 -0500

    UI/UX for user auth

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,10 +5,45 @@ import ScrCreator from './ScrCreator.jsx';
 
 const MainContainer = () => {
   const [displayScrCreator, setDisplay] = useState(false);
+  const [authStr, setAuthStr] = useState('false');
   let myContext = new AudioContext();
+
+  // TODO: finish forms (eg birthdate should be a date selector)
+  const auth = (authStr) => {
+    switch(authStr){
+      case 'signup':
+        return (
+          <div>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <input type="password" placeholder="confirm password"></input>
+            <input type="text" placeholder="birthdate: YYY-MM-DD"></input>
+            <button>Sign up</button>
+          </div>
+        );
+      case 'login':
+        return (
+          <div>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Log in</button>
+          </div>
+        );
+      default:
+        return '';
+    }
+  }
 
   return (
     <div className="mainWrapper">
+      <div className="banner">
+      {auth(authStr)}
+        <select defaultValue="" placeholder="" onChange={(event) => setAuthStr(event.target.value)}>
+        <option value="" disabled>log in/sign up</option>
+          <option value="login">log in</option>
+          <option value="signup">sign up</option>
+        </select>
+      </div>
       <div className="MainContainer">
         <header className="MainContainer-header">
           <h1>Scription</h1>
