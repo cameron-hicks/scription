9230ce355449ded6158d2c53f7b8f645aff7628d
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-01-13 23:05:30 -0500

    setting up onChange and onClick fns

--- src/components/ScrCreator.jsx
+++ src/components/ScrCreator.jsx
@@ -10,16 +10,16 @@ const ScrCreator = () => {
   return (
     <div className="ScrCreator">
       <form>
-        <input id="username-input" type="text" placeholder="username"></input>
-        <input id="title-input" type="text" placeholder="song title"></input>
-        <select id="genre-input" defaultValue="">
+        <input id="username-input" type="text" placeholder="username" onChange={() => setName(this.value)}></input>
+        <input id="title-input" type="text" placeholder="song title" onChange={() => setTitle(this.value)}></input>
+        <select id="genre-input" defaultValue="" onChange={() => setGenre(this.value)}>
           <option value="" disabled>genre</option>
           <option value="folk">folk</option>
           <option value="pop">pop</option>
           <option value="movie music">movie music</option>
           <option value="other">other</option>
         </select>
-        <textarea spellCheck="false" placeholder="Add your transcription in ABC notation..."></textarea>
+        <textarea spellCheck="false" placeholder="Add your transcription in ABC notation..." onChange={() => setAbc(this.value)}></textarea>
         <div className="right-align-btns">
           <button onClick={() => console.log(newName, newTitle, newGenre, newAbc)}>
             post <i className="fas fa-share-square"></i>
