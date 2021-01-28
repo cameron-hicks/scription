6d5eaf72f3bb0b1bb6bd17bfc1f460b256462d02
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-01-27 23:44:21 -0500

    better birthday input

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,7 +10,13 @@ const Signup = () => {
         <input type="text" placeholder="username"></input>
         <input type="password" placeholder="password"></input>
         <input type="password" placeholder="confirm password"></input>
-        <input type="text" placeholder="birthdate: YYY-MM-DD"></input>
+        <label>Birthday:
+          <div>
+            <input type="text" className="birthday" id="year" maxLength="4" placeholder="YYYY"></input>
+            <input type="text" className="birthday" id="month" maxLength="2" placeholder="MM"></input>
+            <input type="text" className="birthday" id="day" maxLength="4" placeholder="DD"></input>
+          </div>
+        </label>
         <button>Sign up</button>
       </form>
     </div>
@@ -18,3 +24,13 @@ const Signup = () => {
 }
 
 export default Signup;
+
+
+/*
+<p> Your birthday: </p>
+          <div>
+            <input type="text" placeholder="YYYY"></input>
+            <input type="text" placeholder="MM"></input>
+            <input type="text" placeholder="DD"></input>
+          </div>
+*/
