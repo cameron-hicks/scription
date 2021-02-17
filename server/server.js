b89976e9fce2d72ff34e4186c036ec7b6ebafe8a
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-02-17 10:59:03 -0500

    adjust production start script for heroku

--- server/server.js
+++ server/server.js
@@ -6,18 +6,18 @@ const apiRouter = require('./routes/api');
 const authRouter = require('./routes/auth');
 
 const app = express();
-
-var corsOptions = {
+const {PORT} = process.env;
+const corsOptions = {
   origin: "http://localhost:8080"   // TODO
 };
 
-// const { COOKIE_SIG } = process.env;
 app.use(cors(corsOptions));
 // 11 middleware for more-secure headers
 // app.use(helmet());
 // parse request bodies of content-type application/json
 app.use(express.json());
 // TODO: use signed cookies
+// const { COOKIE_SIG } = process.env;
 // app.use(cookieParser(COOKIE_SIG));
 app.use(cookieParser());
 // statically serve everything in the build folder 
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
